98a29fe2392092c6aa63511ca73a00eaa700a663
Stefan Van Dyck <vandyck.stefan@gmail.com>
2024-10-24 16:40:31 +0200

    #61: allow building a version of the front-end static pages for use with the local running environment

--- branding/app/js/settings.js
+++ branding/app/js/settings.js
@@ -1,30 +1,44 @@
-const baseDomain = 'biodiversiteitsportaal.dev.svdev.be';
+console.log(`Building static pages for ${process.env.NODE_ENV} environment`)
+
+let protocol = 'https';
+let baseDomain = 'biodiversiteitsportaal.dev.svdev.be';
+let brandingDomain = baseDomain
+let loginUrl = "https://biodiversiteitsportaal-dev.auth.eu-west-1.amazoncognito.com/login?client_id=7072p1h0hnf2hiu7172iuqjsbb&response_type=code&scope=openid&redirect_uri=https%3A%2F%2Fwww.biodiversiteitsportaal.dev.svdev.be?auth-cookie-action=set"
+let logoutUrl = "https://biodiversiteitsportaal-dev.auth.eu-west-1.amazoncognito.com/logout?client_id=7072p1h0hnf2hiu7172iuqjsbb&logout_uri=https%3A%2F%2Fwww.biodiversiteitsportaal.dev.svdev.be?auth-cookie-action=remove"
+
+if (process.env.NODE_ENV === 'docker') {
+    protocol = 'http';
+    baseDomain = 'la-flanders.org:8080';
+    brandingDomain = 'la-flanders.org';
+    loginUrl = "http://localhost:9999/default/authorize?client_id=7072p1h0hnf2hiu7172iuqjsbb&response_type=code&scope=openid&redirect_uri=http%3A%2F%2Fwww.la-flanders.org?auth-cookie-action=remove"
+    logoutUrl = "http://localhost:9999/default/endsession?post_logout_redirect_uri=http%3A%2F%2Fwww.la-flanders.org?auth-cookie-action=remove"
+}
 
 module.exports = {
   isDevel: false,
   inMante: false, // set to true and deploy if you want to set a maintenance message in all the services
   enabledLangs: ['nl', 'en'],
-  mainDomain: baseDomain, // used for cookies (without http/https)
-  mainLAUrl: `https://www.${baseDomain}`,
-  baseFooterUrl: `https://branding.${baseDomain}`,
+  mainDomain: brandingDomain, // used for cookies (without http/https)
+  mainLAUrl: `${protocol}://www.${brandingDomain}`,
+  baseFooterUrl: `${protocol}://${brandingDomain}`,
   theme: 'vlaanderen', // for now 'material', 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
   services: {
-    collectory: { url: `https://collections.${baseDomain}`, title: 'Collections' },
-    biocache: { url: `https://records.${baseDomain}`, title: 'Occurrence records' },
-    biocacheService: { url: `https://records-ws.${baseDomain}`, title: 'Occurrence records webservice' },
-    bie: { url: `https://species.${baseDomain}`, title: 'Species' },
+    collectory: { url: `${protocol}://collections.${baseDomain}`, title: 'Collections' },
+    biocache: { url: `${protocol}://records.${baseDomain}`, title: 'Occurrence records' },
+    biocacheService: { url: `${protocol}://records-ws.${baseDomain}`, title: 'Occurrence records webservice' },
+    bie: { url: `${protocol}://species.${baseDomain}`, title: 'Species' },
     // This bieService var is used by the search autocomplete. With your BIE
-    bieService: { url: `https://species-ws.${baseDomain}`, title: 'Species webservice' },
-    regions: { url: `https://regions.${baseDomain}`, title: 'Regions' },
-    lists: { url: `https://lists.${baseDomain}`, title: 'Species List' },
-    spatial: { url: `https://spatial.${baseDomain}`, title: 'Spatial Portal' },
-    images: { url: `https://images.${baseDomain}`, title: 'Images Service' },
-    cas: { url: `https://auth.${baseDomain}`, title: 'CAS' }
+    bieService: { url: `${protocol}://species-ws.${baseDomain}`, title: 'Species webservice' },
+    regions: { url: `${protocol}://regions.${baseDomain}`, title: 'Regions' },
+    lists: { url: `${protocol}://lists.${baseDomain}`, title: 'Species List' },
+    spatial: { url: `${protocol}://spatial.${baseDomain}`, title: 'Spatial Portal' },
+    images: { url: `${protocol}://images.${baseDomain}`, title: 'Images Service' },
+    cas: { url: `${protocol}://auth.${baseDomain}`, title: 'CAS' }
   },
-  loginUrl: "https://biodiversiteitsportaal-dev.auth.eu-west-1.amazoncognito.com/login?client_id=7072p1h0hnf2hiu7172iuqjsbb&response_type=code&scope=openid&redirect_uri=https%3A%2F%2Fwww.biodiversiteitsportaal.dev.svdev.be?auth-cookie-action=set",
-  logoutUrl: "https://biodiversiteitsportaal-dev.auth.eu-west-1.amazoncognito.com/logout?client_id=7072p1h0hnf2hiu7172iuqjsbb&logout_uri=https%3A%2F%2Fwww.biodiversiteitsportaal.dev.svdev.be?auth-cookie-action=remove",
+  loginUrl: loginUrl,
+  logoutUrl: logoutUrl,
   otherLinks: [
-    { title: 'Datasets', url: `https://collections.${baseDomain}/datasets` },
-    { title: 'Explore your area', url: `https://records.${baseDomain}/explore/your-area/` }
+    { title: 'Datasets', url: `${protocol}://collections.${baseDomain}/datasets` },
+    { title: 'Explore your area', url: `${protocol}://records.${baseDomain}/explore/your-area/` }
   ]
 }
