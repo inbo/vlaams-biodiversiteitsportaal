8ac5073759af0f3ab499b0f86c33c31531121edb
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-02-24 15:56:46 +0100

    Fix port-forwarding script on linux. Port was only available on 127.0.0.1

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -10,6 +10,14 @@ INSTANCE_ID=$(aws ec2 describe-instances \
                --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]" \
                --output text)
 
+# Issue with ssm only binding on 127.0.0.1, making the port unavailable to docker containers
+# https://github.com/aws/session-manager-plugin/issues/14
+# Fixed by forwarding using socat on a port with a 1 pre-pended
+if [[ "$(uname -s)" == "Linux" ]]; then
+  socat TCP-LISTEN:${LOCAL_PORT},fork,reuseaddr TCP:127.0.0.1:1${LOCAL_PORT} &
+  LOCAL_PORT="1${LOCAL_PORT}"
+fi
+
 # create the port forwarding tunnel
 aws ssm start-session --target "${INSTANCE_ID}" \
                        --document-name AWS-StartPortForwardingSessionToRemoteHost \
