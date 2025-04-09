8dcabaabdaf400cc51667295f489888bf8d28c1d
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-04-09 13:25:28 +0200

    185: add autocomplete config

--- branding/src/portal/ala/autocomplete.ts
+++ branding/src/portal/ala/autocomplete.ts
@@ -1,3 +1,17 @@
+import "../../settings";
+
+declare global {
+    interface Window {
+        BC_CONF: any;
+    }
+}
+window.BC_CONF = {
+    autoCompleteSelector: "#autocompleteHeader",
+    appendToSelector: "#autocompleteSearchALA",
+    autocompleteURL: "/bie-index/ws/search/auto.json",
+    templateId: "autoCompleteTemplate",
+};
+
 import "jquery-ui";
 import "jquery-ui/ui/widgets/autocomplete";
-import "jquery-ui/themes/base/autocomplete.css"
+import "jquery-ui/themes/base/autocomplete.css";
