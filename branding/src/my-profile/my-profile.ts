4c031d73f9baf57ff0664246e72efbf28ad47a59
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-05-13 16:21:58 +0200

    #49: Fix issue with user-details login state requiring a reload

--- branding/src/my-profile/my-profile.ts
+++ branding/src/my-profile/my-profile.ts
@@ -11,61 +11,64 @@ $(() => {
 });
 
 async function initPage() {
+    getUserManagerInstance().events.addUserLoaded(async (user) => {
+        showUserDetails(user);
+    });
     if (await isLoggedIn()) {
         const user = await getUser();
         if (user) {
             showUserDetails(user);
-            return;
         }
     }
-    getUserManagerInstance().events.addUserLoaded(async (user) => {
-        showUserDetails(user);
-    });
 }
 
+let showDetailsRan = false;
 function showUserDetails(user: User) {
-    console.trace("User loaded", user);
-    const profile = user!.profile!;
+    if (!showDetailsRan) {
+        showDetailsRan = true;
+        console.trace("User loaded", user);
+        const profile = user!.profile!;
 
-    document.getElementById("user-greeting-name")!
-        .innerText = `${profile.given_name} ${profile.family_name}`;
+        document.getElementById("user-greeting-name")!
+            .innerText = `${profile.given_name} ${profile.family_name}`;
 
-    (document.getElementById(
-        "update-profile-details",
-    )! as HTMLAnchorElement).addEventListener("click", (e) => {
-        e.preventDefault();
-        login({ "kc_action": "UPDATE_PROFILE" });
-    });
-
-    (document.getElementById("update-password")! as HTMLAnchorElement)
-        .addEventListener("click", (e) => {
+        (document.getElementById(
+            "update-profile-details",
+        )! as HTMLAnchorElement).addEventListener("click", (e) => {
             e.preventDefault();
-            login({ "kc_action": "UPDATE_PASSWORD" });
+            login({ "kc_action": "UPDATE_PROFILE" });
         });
 
-    (document.getElementById("my-annotated-records")! as HTMLAnchorElement)
-        .href =
-            `/biocache-hub/occurrences/search/?q=*:*&amp;fq=assertion_user_id:%22${profile.sub}%22`;
+        (document.getElementById("update-password")! as HTMLAnchorElement)
+            .addEventListener("click", (e) => {
+                e.preventDefault();
+                login({ "kc_action": "UPDATE_PASSWORD" });
+            });
 
-    const roles: string[] = (profile.realm_access as any)?.roles || [];
+        (document.getElementById("my-annotated-records")! as HTMLAnchorElement)
+            .href =
+                `/biocache-hub/occurrences/search/?q=*:*&amp;fq=assertion_user_id:%22${profile.sub}%22`;
 
-    const rolesElement = document.getElementById("my-roles")!;
-    roles.forEach((role) => {
-        const roleElement = document.createElement("span");
-        roleElement.className = "badge badge-secondary";
-        roleElement.innerText = role;
-        rolesElement.appendChild(roleElement);
-    });
+        const roles: string[] = (profile.realm_access as any)?.roles || [];
 
-    if (roles.includes("ADMIN")) {
-        Array.from(document.getElementsByClassName("admin-tooling"))
-            .forEach(
-                (element) => {
-                    element.classList.remove("hidden");
-                },
-            );
+        const rolesElement = document.getElementById("my-roles")!;
+        roles.forEach((role) => {
+            const roleElement = document.createElement("span");
+            roleElement.className = "badge badge-secondary";
+            roleElement.innerText = role;
+            rolesElement.appendChild(roleElement);
+        });
+
+        if (roles.includes("ADMIN")) {
+            Array.from(document.getElementsByClassName("admin-tooling"))
+                .forEach(
+                    (element) => {
+                        element.classList.remove("hidden");
+                    },
+                );
+        }
+
+        document.getElementById("profile-login")!.classList.add("hidden");
+        document.getElementById("profile-overview")!.classList.remove("hidden");
     }
-
-    document.getElementById("profile-login")!.classList.add("hidden");
-    document.getElementById("profile-overview")!.classList.remove("hidden");
 }
