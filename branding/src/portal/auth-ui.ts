d7f555bbffdf6bacc63082a273ea2bc55aa5ecaa
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-07-16 21:13:41 +0200

    Fix auth error

--- branding/src/portal/auth-ui.ts
+++ branding/src/portal/auth-ui.ts
@@ -35,16 +35,16 @@ export async function handleAuthCallbacks() {
 export async function setAuthMenuStatus() {
     Array.from(document.getElementsByClassName("login-status-dependent"))
         .forEach(async (element) => {
-            if (await isLoggedIn()) {
+            const user = await getUser();
+            if (user !== null && await isLoggedIn()) {
                 element.classList.remove(settings.auth.ala.logoutClass);
                 element.classList.add(settings.auth.ala.loginClass);
 
-                const profile = (await getUser())!.profile;
                 (document.getElementById(
                     "my-annotated-records",
                 )! as HTMLAnchorElement)
                     .href =
-                        `/biocache-hub/occurrences/search/?q=*:*&fq=assertion_user_id:%22${profile.sub}%22`;
+                        `/biocache-hub/occurrences/search/?q=*:*&fq=assertion_user_id:%22${user.profile.sub}%22`;
             } else {
                 element.classList.remove(settings.auth.ala.loginClass);
                 element.classList.add(settings.auth.ala.logoutClass);
