f0cae1b097243fd1d429b5bcc05349ba8d22dc38
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-09 16:07:53 +0200

    #438: Add missing file

---
+++ branding/src/monitoring/aws-rum.ts
@@ -0,0 +1,22 @@
+import { AwsRum } from "aws-rum-web";
+import settings from "../settings";
+
+if (process.env.NODE_ENV !== "development") {
+    try {
+        new AwsRum(
+            settings.monitoring.awsRumAppId,
+            settings.appVersion,
+            settings.monitoring.awsRegion,
+            {
+                sessionSampleRate: 0.1,
+                endpoint: "https://dataplane.rum.eu-west-1.amazonaws.com",
+                telemetries: [],
+                allowCookies: true,
+                enableXRay: true,
+                signing: true, // If you have a public resource policy and wish to send unsigned requests please set this to false
+            },
+        );
+    } catch (error) {
+        // Ignore errors thrown during CloudWatch RUM web client initialization
+    }
+}
