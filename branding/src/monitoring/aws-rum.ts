e6322a121a25d3fbb378ddbb0bc52045e0c25f39
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-10 08:50:14 +0200

    #438: fix rum client

--- branding/src/monitoring/aws-rum.ts
+++ branding/src/monitoring/aws-rum.ts
@@ -1,17 +1,18 @@
 import { AwsRum } from "aws-rum-web";
 import settings from "../settings";
 
+let rumClient: AwsRum | undefined = undefined;
 if (process.env.NODE_ENV !== "development") {
     try {
-        new AwsRum(
+        rumClient = new AwsRum(
             settings.monitoring.awsRumAppId,
             settings.appVersion,
             settings.monitoring.awsRegion,
             {
                 sessionSampleRate: 0.1,
                 endpoint: "https://dataplane.rum.eu-west-1.amazonaws.com",
-                telemetries: [],
-                allowCookies: true,
+                telemetries: ["performance", "errors", "http"],
+                allowCookies: false,
                 enableXRay: true,
                 signing: true, // If you have a public resource policy and wish to send unsigned requests please set this to false
             },
@@ -20,3 +21,5 @@ if (process.env.NODE_ENV !== "development") {
         // Ignore errors thrown during CloudWatch RUM web client initialization
     }
 }
+
+export { rumClient };
