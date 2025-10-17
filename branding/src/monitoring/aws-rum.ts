06290f48ac2afd8ba1f767d10cc474d728f31bea
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-17 12:14:24 +0200

    Fix async rum init

--- branding/src/monitoring/aws-rum.ts
+++ branding/src/monitoring/aws-rum.ts
@@ -2,40 +2,43 @@ import { AwsRum } from "aws-rum-web";
 import settings from "../settings";
 import { userManagerPromise } from "../auth/auth";
 
-let rumClient: AwsRum | undefined = undefined;
-if (process.env.NODE_ENV !== "development") {
-    try {
-        console.info("Initializing CloudWatch RUM web client");
-        rumClient = new AwsRum(
-            settings.monitoring.awsRumAppId,
-            settings.appVersion,
-            settings.monitoring.awsRegion,
-            {
-                enableRumClient: true,
-                sessionSampleRate: 0.1,
-                endpoint: settings.monitoring.awsRumEndpoint,
-                telemetries: ["performance", "errors", ["http", {
-                    addXRayTraceIdHeader: true,
-                }]],
-                allowCookies: false,
-                enableXRay: true,
-                signing: true,
-                identityPoolId: settings.monitoring.awsCognitoIdentityPoolId,
-            },
-        );
+let rumClient: Promise<AwsRum | undefined>;
 
-        const userManager = await userManagerPromise;
-        userManager.events.addUserLoaded((user) => {
-            if (user && rumClient) {
-                rumClient.addSessionAttributes({
-                    userId: user.profile.sub,
-                });
-                rumClient.allowCookies(true);
-            }
-        });
-    } catch (error) {
-        // Ignore errors thrown during CloudWatch RUM web client initialization
-    }
+async function initRum() {
+    console.info("Initializing CloudWatch RUM web client");
+    const client = new AwsRum(
+        settings.monitoring.awsRumAppId,
+        settings.appVersion,
+        settings.monitoring.awsRegion,
+        {
+            enableRumClient: true,
+            sessionSampleRate: 0.1,
+            endpoint: settings.monitoring.awsRumEndpoint,
+            telemetries: ["performance", "errors", ["http", {
+                addXRayTraceIdHeader: true,
+            }]],
+            allowCookies: false,
+            enableXRay: true,
+            signing: true,
+            identityPoolId: settings.monitoring.awsCognitoIdentityPoolId,
+        },
+    );
+
+    const userManager = await userManagerPromise;
+    userManager.events.addUserLoaded((user) => {
+        if (user && client) {
+            client.addSessionAttributes({
+                userId: user.profile.sub,
+            });
+            client.allowCookies(true);
+        }
+    });
+
+    return client;
+}
+
+if (process.env.NODE_ENV !== "development") {
+    rumClient = initRum();
 }
 
 export { rumClient };
