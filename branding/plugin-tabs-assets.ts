2f63ea1973c83b94dc6e6cac10e2dc5f71daebb4
kshaleninova <katya@gleveconsult.com>
2025-10-22 13:11:30 +0200

    #724 - update bie-hub - configure 'faunabeheer' plugin tab for bie-hub - refactor script to download assets for plugin tabs (abv, faunabeheer)

---
+++ branding/plugin-tabs-assets.ts
@@ -0,0 +1,25 @@
+import fs from "fs/promises";
+import decompress from "decompress";
+import { open } from "fs/promises";
+
+export async function downloadSpeciesPluginTabAssets(
+  { outputFolder, assetsS3RelativePath , assetsFilepath  }:
+  { outputFolder: string, assetsS3RelativePath: string, assetsFilepath: string },
+): Promise<void> {
+
+  try {
+    await open(assetsFilepath);
+  } catch (e) {
+    console.log(assetsFilepath + " not found, downloading...");
+
+    const response = await fetch(
+      "https://inbo-vbp-global-public-artifacts.s3.eu-west-1.amazonaws.com/" + assetsS3RelativePath,
+    );
+    const buffer = Buffer.from(await response.arrayBuffer());
+    await fs.writeFile(assetsFilepath, buffer);
+    console.log(assetsS3RelativePath + " assets downloaded from S3 successfully and written into " + assetsFilepath);
+  }
+
+  await decompress(assetsFilepath, outputFolder);
+  console.log(assetsFilepath + " assets unzipped successfully into " + outputFolder);
+}
