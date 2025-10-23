8657109ad9935a23a5a219d40352713094d4fbb0
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-23 12:33:01 +0200

    Added a simple bash script to help maintain grafana provisioned data for the different environments

---
+++ config/grafana/template-provisioning.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# This (very basic) script generates environment-specific Grafana provisioning files
+# by templating existing "dev" files for "uat" and "prod" environments.
+
+function template() {
+  file="$1"
+  env="$2"
+  newFile="${file//dev/"$env"}"
+  newFile="${newFile//DEV/"${env^^}"}"
+  echo "Generating $newFile from $file"
+
+  mkdir -p "$(dirname "$newFile")"
+  sed -e "s/-dev/-${env}/g" "$file" > "$newFile"
+  sed -i -e "s/DEV/${env^^}/g" "$newFile"
+}
+
+# Loop over all files in provisioning directory and its subdirectories
+while IFS= LC_ALL=C read -r -d '' -u 9 file
+do
+  # If the filepath contains "dev" (case insensitive), template it for other environments
+  if [[ "${file,,}" == *"dev"* ]]; then
+    for env in "uat" "prod"; do
+      template "$file" "$env"
+    done
+  fi
+done 9< <( find ./provisioning -type f -exec printf '%s\0' {} + )
