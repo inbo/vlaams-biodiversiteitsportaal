b2376d279c4995f7b26182c4b85c7f8fbf267bf8
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-12-16 14:53:10 +0100

    Remove offline-access scope, as it does not create a proper SSO session

--- branding/src/auth/auth.ts
+++ branding/src/auth/auth.ts
@@ -1,10 +1,10 @@
 import settings from "../settings";
 import {
-  Log,
-  SigninRedirectArgs,
-  SignoutRedirectArgs,
-  User,
-  UserManager,
+    Log,
+    SigninRedirectArgs,
+    SignoutRedirectArgs,
+    User,
+    UserManager,
 } from "oidc-client-ts";
 
 import Cookies from "js-cookie";
@@ -24,195 +24,179 @@ const authServiceWorker = new AuthServiceWorker();
 export const userManagerPromise = initUserManager(authServiceWorker);
 
 async function initUserManager(
-  authServiceWorker: AuthServiceWorker,
+    authServiceWorker: AuthServiceWorker,
 ): Promise<UserManager> {
-  console.debug("Initializing OIDC UserManager");
-  authServiceWorker.reset();
+    console.debug("Initializing OIDC UserManager");
+    authServiceWorker.reset();
 
-  const redirectUrl = cleanupUrl(window.location.href);
-  redirectUrl.searchParams.set("front-auth-action", "login");
+    const redirectUrl = cleanupUrl(window.location.href);
+    redirectUrl.searchParams.set("front-auth-action", "login");
 
-  const post_logout_redirect_uri = cleanupUrl(window.location.href);
-  post_logout_redirect_uri.searchParams.set("front-auth-action", "logout");
+    const post_logout_redirect_uri = cleanupUrl(window.location.href);
+    post_logout_redirect_uri.searchParams.set("front-auth-action", "logout");
 
-  const manager = new UserManager({
-    authority: settings.auth.oidc.authority,
-    client_id: settings.auth.oidc.clientId,
-    redirect_uri: redirectUrl.href,
-    post_logout_redirect_uri: post_logout_redirect_uri.href,
-    scope: "openid email ala/roles offline_access",
-    includeIdTokenInSilentSignout: true,
-    prompt: settings.auth.oidc.prompt,
-    silent_redirect_uri: `${settings.domain}?front-auth-action=login`,
-    automaticSilentRenew: true,
-    monitorSession: false,
-  });
+    const manager = new UserManager({
+        authority: settings.auth.oidc.authority,
+        client_id: settings.auth.oidc.clientId,
+        redirect_uri: redirectUrl.href,
+        post_logout_redirect_uri: post_logout_redirect_uri.href,
+        scope: "openid email ala/roles",
+        includeIdTokenInSilentSignout: true,
+        prompt: settings.auth.oidc.prompt,
+        silent_redirect_uri: `${settings.domain}?front-auth-action=login`,
+        automaticSilentRenew: true,
+        monitorSession: false,
+    });
 
-  window.addEventListener(
-    "focus",
-    async function() {
-      manager.startSilentRenew();
-      await manager.getUser(true);
-    },
-    false,
-  );
-  window.addEventListener(
-    "blur",
-    async function() {
-      manager.stopSilentRenew();
-    },
-    false,
-  );
+    manager.events.addUserLoaded(async (user) => {
+        console.log("User loaded", user);
+        setAlaAuthCookie(user);
+        await authServiceWorker.setAccessToken(user);
+    });
+    manager.events.addUserUnloaded(async () => {
+        console.log("User unloaded");
+        clearAlaAuthCookies();
+        await authServiceWorker.setAccessToken(null);
+    });
 
-  manager.events.addUserLoaded(async (user) => {
-    console.log("User loaded", user);
-    setAlaAuthCookie(user);
-    await authServiceWorker.setAccessToken(user);
-  });
-  manager.events.addUserUnloaded(async () => {
-    console.log("User unloaded");
-    clearAlaAuthCookies();
-    await authServiceWorker.setAccessToken(null);
-  });
+    manager.events.addAccessTokenExpired(async function () {
+        console.warn("Access token expired");
+        if (!document.hidden) {
+            await silentLogin(manager);
+        }
+    });
+    manager.events.addSilentRenewError(async (user) => {
+        console.error("Silent renew error", user);
+        await manager.removeUser();
+    });
 
-  manager.events.addAccessTokenExpired(async function() {
-    console.warn("Access token expired");
-    if (!document.hidden) {
-      await silentLogin(manager);
-    }
-  });
-  manager.events.addSilentRenewError(async (user) => {
-    console.error("Silent renew error", user);
-    await manager.removeUser();
-  });
+    await handleAuthCallbacks(manager);
 
-  await handleAuthCallbacks(manager);
-
-  const user = await manager.getUser();
-  if (Cookies.get(settings.auth.ala.authCookieName)) {
-    if (!user) {
-      await manager.signinRedirect();
+    const user = await manager.getUser();
+    if (Cookies.get(settings.auth.ala.authCookieName)) {
+        if (!user) {
+            await manager.signinRedirect();
+        } else {
+            await authServiceWorker.setAccessToken(user);
+        }
     } else {
-      await authServiceWorker.setAccessToken(user);
+        if (user) {
+            await manager.removeUser();
+        } else {
+            await authServiceWorker.setAccessToken(null);
+        }
     }
-  } else {
-    if (user) {
-      await manager.removeUser();
-    } else {
-      await authServiceWorker.setAccessToken(null);
-    }
-  }
 
-  return manager;
+    return manager;
 }
 
 async function handleAuthCallbacks(manager: UserManager) {
-  const urlParams = new URLSearchParams(window.location.search);
+    const urlParams = new URLSearchParams(window.location.search);
 
-  const frontAuthAction = urlParams.get("front-auth-action");
-  if (frontAuthAction) {
-    switch (frontAuthAction) {
-      case "login":
-        await manager.signinCallback();
-        await manager.clearStaleState();
-        break;
-      case "logout":
-        await manager.signoutCallback();
-        await manager.clearStaleState();
-        break;
+    const frontAuthAction = urlParams.get("front-auth-action");
+    if (frontAuthAction) {
+        switch (frontAuthAction) {
+            case "login":
+                await manager.signinCallback();
+                await manager.clearStaleState();
+                break;
+            case "logout":
+                await manager.signoutCallback();
+                await manager.clearStaleState();
+                break;
+        }
+
+        window.history.pushState(
+            null,
+            document.title,
+            cleanupUrl(window.location.href),
+        );
     }
-
-    window.history.pushState(
-      null,
-      document.title,
-      cleanupUrl(window.location.href),
-    );
-  }
 }
 
 async function silentLogin(manager: UserManager) {
-  try {
-    await manager.signinSilent({ silentRequestTimeoutInSeconds: 2 });
-  } catch (error) {
-    console.error("Silent login failed", error);
-    await manager.removeUser();
-  }
+    try {
+        await manager.signinSilent({ silentRequestTimeoutInSeconds: 2 });
+    } catch (error) {
+        console.error("Silent login failed", error);
+        await manager.removeUser();
+    }
 }
 
 function setAlaAuthCookie(user?: User) {
-  Cookies.set(settings.auth.ala.authCookieName, user?.profile?.sub || "vbp", {
-    path: "/",
-    sameSite: "lax",
-    secure: window.location.protocol === "https:",
-    domain: settings.auth.ala.authCookieDomain,
-  });
+    Cookies.set(settings.auth.ala.authCookieName, user?.profile?.sub || "vbp", {
+        path: "/",
+        sameSite: "lax",
+        secure: window.location.protocol === "https:",
+        domain: settings.auth.ala.authCookieDomain,
+    });
 }
 
 function clearAlaAuthCookies() {
-  Cookies.remove(settings.auth.ala.authCookieName, {
-    path: "/",
-    sameSite: "lax",
-    secure: window.location.protocol === "https:",
-    domain: settings.auth.ala.authCookieDomain,
-  });
-  [
-    "alerts",
-    "apikey",
-    "bie-hub",
-    "bie-index",
-    "biocache-hub",
-    "biocache-service",
-    "collectory",
-    "data-quality-filter-service",
-    "image-service",
-    "logger",
-    "regions",
-    "sandbox-hub",
-    "sandbox-service",
-    "sensitive-data-service",
-    "spatial-hub",
-    "spatial-service",
-    "species-list",
-  ].forEach((path) => {
-    Cookies.remove("JSESSIONID", {
-      path: `/${path}`,
-      sameSite: "lax",
-      secure: window.location.protocol === "https:",
+    Cookies.remove(settings.auth.ala.authCookieName, {
+        path: "/",
+        sameSite: "lax",
+        secure: window.location.protocol === "https:",
+        domain: settings.auth.ala.authCookieDomain,
     });
-  });
+    [
+        "alerts",
+        "apikey",
+        "bie-hub",
+        "bie-index",
+        "biocache-hub",
+        "biocache-service",
+        "collectory",
+        "data-quality-filter-service",
+        "image-service",
+        "logger",
+        "regions",
+        "sandbox-hub",
+        "sandbox-service",
+        "sensitive-data-service",
+        "spatial-hub",
+        "spatial-service",
+        "species-list",
+    ].forEach((path) => {
+        Cookies.remove("JSESSIONID", {
+            path: `/${path}`,
+            sameSite: "lax",
+            secure: window.location.protocol === "https:",
+        });
+    });
 }
 
 function cleanupUrl(url: string) {
-  const cleanedUrl = new URL(url);
-  cleanedUrl.searchParams.delete("front-auth-action");
-  cleanedUrl.searchParams.delete("code");
-  cleanedUrl.searchParams.delete("state");
-  cleanedUrl.searchParams.delete("sessionState");
-  cleanedUrl.searchParams.delete("session_state");
-  cleanedUrl.searchParams.delete("iss");
-  return cleanedUrl;
+    const cleanedUrl = new URL(url);
+    cleanedUrl.searchParams.delete("front-auth-action");
+    cleanedUrl.searchParams.delete("code");
+    cleanedUrl.searchParams.delete("state");
+    cleanedUrl.searchParams.delete("sessionState");
+    cleanedUrl.searchParams.delete("session_state");
+    cleanedUrl.searchParams.delete("iss");
+    return cleanedUrl;
 }
 
 export async function login(args?: SigninRedirectArgs | any) {
-  const mgr = await userManagerPromise;
-  clearAlaAuthCookies();
-  await mgr.signinRedirect(args);
+    const mgr = await userManagerPromise;
+    clearAlaAuthCookies();
+    await mgr.signinRedirect(args);
 }
 
 export async function logout(args?: SignoutRedirectArgs | any) {
-  const mgr = await userManagerPromise;
-  clearAlaAuthCookies();
-  await mgr.signoutRedirect(args);
+    const mgr = await userManagerPromise;
+    clearAlaAuthCookies();
+    await mgr.signoutRedirect(args);
 }
 
 export async function getUser(): Promise<User | null> {
-  const mgr = await userManagerPromise;
-  return await mgr.getUser();
+    const mgr = await userManagerPromise;
+    return await mgr.getUser();
 }
 
 export async function isLoggedIn() {
-  return (
-    Cookies.get(settings.auth.ala.authCookieName) &&
-    (await getUser()) !== null
-  );
+    return (
+        Cookies.get(settings.auth.ala.authCookieName) &&
+        (await getUser()) !== null
+    );
 }
