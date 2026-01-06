45037f1408e3fab3995a9fb214a45fe6b1b3ba93
Stefan Van Dyck <vandyck.stefan@gmail.com>
2026-01-06 14:50:20 +0100

    Fix tests

--- test/cypress/e2e/1-main-page/homepage.cy.ts
+++ test/cypress/e2e/1-main-page/homepage.cy.ts
@@ -1,50 +1,50 @@
 describe("Visit the homepage", () => {
-    beforeEach(() => {
-        cy.visit("/");
-    });
+  beforeEach(() => {
+    cy.visit("/");
+  });
 
-    it("Shows a working search bar", () => {
-        const searchInput = "test";
-        cy.get("#index-search").should("be.visible").type(
-            `${searchInput}{enter}`,
-        )
-            .url().should(
-                "include",
-                "/bie-hub/search",
-            ).get("h1").should(
-                "contain.text",
-                `*${searchInput}*`,
-            ).get(".search-results-list").children().should(
-                "have.length.greaterThan",
-                2,
-            );
-    });
+  it("Shows a working search bar", () => {
+    const searchInput = "test";
+    cy.get("#index-search")
+      .should("be.visible")
+      .type(`${searchInput}{enter}`)
+      .url()
+      .should("include", "/bie-hub/search")
+      .get("h1")
+      .should("contain.text", searchInput)
+      .get(".search-results-list")
+      .children()
+      .should("have.length.greaterThan", 2);
+  });
 
-    it("Search bar has working auto-complete", () => {
-        const searchInput = "pica p";
-        cy.get("#index-search").should("be.visible").type(`${searchInput}`)
-            .get(".ui-autocomplete")
-            .should("be.visible")
-            .children().should("have.length", 10);
-        // TODO: Fix order of the results
-        // .first().should("contain.text", "pica pica"); // Closest match is in the middle??
-    });
+  it("Search bar has working auto-complete", () => {
+    const searchInput = "pica p";
+    cy.get("#index-search")
+      .should("be.visible")
+      .type(`${searchInput}`)
+      .get(".ui-autocomplete")
+      .should("be.visible")
+      .children()
+      .should("have.length", 10);
+    // TODO: Fix order of the results
+    // .first().should("contain.text", "pica pica"); // Closest match is in the middle??
+  });
 
-    ["prev", "next"].forEach((value) =>
-        it(`Shows a picture carousel that can be navigated using ${value} button`, () => {
-            cy.get("[data-carousel-active]")
+  ["prev", "next"].forEach((value) =>
+    it(`Shows a picture carousel that can be navigated using ${value} button`, () => {
+      cy.get("[data-carousel-active]")
+        .invoke("css", "background-image")
+        .should("not.equal", "none")
+        .then((initialBgImage) => {
+          cy.get(`.vbp-picture-carousel-button-${value}`)
+            .click()
+            .then(() => {
+              cy.get("[data-carousel-active]")
                 .invoke("css", "background-image")
                 .should("not.equal", "none")
-                .then((initialBgImage) => {
-                    cy.get(`.vbp-picture-carousel-button-${value}`)
-                        .click()
-                        .then(() => {
-                            cy.get("[data-carousel-active]")
-                                .invoke("css", "background-image")
-                                .should("not.equal", "none")
-                                .should("not.equal", initialBgImage);
-                        });
-                });
-        })
-    );
+                .should("not.equal", initialBgImage);
+            });
+        });
+    }),
+  );
 });
--- test/cypress/e2e/2-biocache/occurrence-details.cy.ts
+++ test/cypress/e2e/2-biocache/occurrence-details.cy.ts
@@ -1,20 +1,19 @@
 describe("Biocache - Occurrence Details", () => {
-    it("Can open an occurrence details page", () => {
-        cy.visit("/biocache-hub/occurrences/search?q=");
+  it("Can open an occurrence details page", () => {
+    cy.visit("/biocache-hub/occurrences/search?q=");
 
-        cy.get(".recordRow").should("be.visible").first().find(
-            "a.occurrenceLink",
-        ).click();
+    cy.get(".recordRow")
+      .should("be.visible")
+      .first()
+      .find("a.occurrenceLink")
+      .click();
 
-        cy.url().should(
-            "include",
-            "/biocache-hub/occurrences/",
-        );
+    cy.url().should("include", "/biocache-hub/occurrences/");
 
-        cy.get("h1").should("be.visible").contains("waarnemingsrecord");
-        cy.get("#occurrenceDataset").should("be.visible");
-        cy.get("#occurrenceEvent").should("be.visible");
-        cy.get("#occurrenceTaxonomy").should("be.visible");
-        cy.get("#occurrenceGeospatial").should("be.visible");
-    });
+    cy.get("h1").should("be.visible").contains("Waarneming");
+    cy.get("#occurrenceDataset").should("be.visible");
+    cy.get("#occurrenceEvent").should("be.visible");
+    cy.get("#occurrenceTaxonomy").should("be.visible");
+    cy.get("#occurrenceGeospatial").should("be.visible");
+  });
 });
