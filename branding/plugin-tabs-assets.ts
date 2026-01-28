063c2d1fa40620449f3da14db262207c6f887d1f
Stefan Van Dyck <vandyck.stefan@gmail.com>
2026-01-28 10:04:15 +0100

    inbo-aws-biodiversiteitsportaal-terraform#2403: Put cloudfront in front of public artifacts s3

--- branding/plugin-tabs-assets.ts
+++ branding/plugin-tabs-assets.ts
@@ -3,13 +3,15 @@ import decompress from "decompress";
 import { open } from "fs/promises";
 import { basename, dirname } from "path";
 
-export async function downloadSpeciesPluginTabAssets(
-  { outputFolder, assetsS3RelativePath, assetsFilepath }: {
-    outputFolder: string;
-    assetsS3RelativePath: string;
-    assetsFilepath: string;
-  },
-): Promise<Map<string, string>> {
+export async function downloadSpeciesPluginTabAssets({
+  outputFolder,
+  assetsS3RelativePath,
+  assetsFilepath,
+}: {
+  outputFolder: string;
+  assetsS3RelativePath: string;
+  assetsFilepath: string;
+}): Promise<Map<string, string>> {
   try {
     const fileHandle = await open(assetsFilepath);
     await fileHandle.close();
@@ -17,15 +19,14 @@ export async function downloadSpeciesPluginTabAssets(
     console.log(assetsFilepath + " not found, downloading...");
 
     const response = await fetch(
-      "https://inbo-vbp-global-public-artifacts.s3.eu-west-1.amazonaws.com/" +
-        assetsS3RelativePath,
+      "https://build.natuurdata.dev.inbo.be/" + assetsS3RelativePath,
     );
     const buffer = Buffer.from(await response.arrayBuffer());
     await fs.writeFile(assetsFilepath, buffer);
     console.log(
       assetsS3RelativePath +
-        " assets downloaded from S3 successfully and written into " +
-        assetsFilepath,
+      " assets downloaded from S3 successfully and written into " +
+      assetsFilepath,
     );
   }
 
@@ -33,7 +34,6 @@ export async function downloadSpeciesPluginTabAssets(
   console.log(
     assetsFilepath + " assets unzipped successfully into " + outputFolder,
   );
-
 
   const files = await fs.readdir(outputFolder);
   return files.reduce((acc, file) => {
