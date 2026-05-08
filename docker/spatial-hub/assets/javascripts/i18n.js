951c5b5be260bb6a846fdc2e8a4b66bd51489567
Stefan Van Dyck <vandyck.stefan@gmail.com>
2026-05-08 09:53:25 +0200

    #923: Fix showing correct language on initial load

--- docker/spatial-hub/assets/javascripts/i18n.js
+++ docker/spatial-hub/assets/javascripts/i18n.js
@@ -1,10 +1,9 @@
 $(function () {
-  if ($('#dropdown-lang').length) {
-    var defaultLang = 'EN';//todo should come from config value
+  if ($("#dropdown-lang").length) {
+    var defaultLang = ($sh ? $sh.i18n : undefined) || "nl";
     var currentLang = new URLSearchParams(document.location.search).get("lang");
-    var lang = currentLang || defaultLang
-    console.log('Language set: ' + lang);
-    $('#lang-switch').html(lang.toUpperCase() + ' <span class="caret"></span>');
+    var lang = currentLang || defaultLang;
+    console.log("Language set: " + lang);
+    $("#lang-switch").html(lang.toUpperCase() + ' <span class="caret"></span>');
   }
 });
-
