2d187543e660246d2a5e58a51037e629d2e3607f
Stefan Van Dyck <vandyck.stefan@gmail.com>
2026-07-07 13:41:39 +0200

    #1062: Add e2e test for creating environmental range layers

--- test/cypress/e2e/4-spatial/area.cy.ts
+++ test/cypress/e2e/4-spatial/area.cy.ts
@@ -30,4 +30,33 @@ describe("Spatial - Layers", () => {
     cy.get("[testtag='nextInNewAreaLegend']").click();
     cy.get(".progress-bar", { timeout: 10_000 }).should("not.be.visible");
   });
+
+  it.only("Can add an environmental range", () => {
+    // Add a layer
+    cy.get("#menu-0").click();
+    cy.get('ul.dropdown-menu[aria-labelledby="menu-0"')
+      .contains("Gebied")
+      .click();
+    cy.get('input[value="environmentalEnvelope"]').check();
+    cy.get('button[name="next"]').click();
+
+    // Select an environmental range
+    cy.get('input[testtag="searchLayer"]').type("neerslag");
+
+    cy.get(".autocomplete-item").first().click();
+
+    cy.get('input[ng-model="selection.min"]').clear().type("800");
+    cy.get('button[testtag="next"]').click();
+
+    cy.get(".progress-bar", { timeout: 10_000 }).should("not.be.visible");
+    cy.get('button[name="closeNewWindow"]').click();
+
+    // Verify that the layer is visible on the map
+    if (Cypress.env("TARGET_ENV") === "prod") {
+      cy.get("#map").matchImageSnapshot({
+        failureThreshold: 0.4,
+        failureThresholdType: "percent",
+      });
+    }
+  });
 });
